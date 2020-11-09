f840ab80dd2f9769b3f611b3f89d7ba3487d8f63
Ray <ottotruepro@gmail.com>
2020-11-09 11:03:12 +0700

    Handle 404 error

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Count
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post, Tag
 
 
@@ -43,7 +43,7 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.select_related('author').get(slug=slug)
+    post = get_object_or_404(Post.objects.select_related('author'), slug=slug)
     comments = post.comments.prefetch_related('author')
     serialized_comments = []
     for comment in comments:
@@ -82,7 +82,7 @@ def post_detail(request, slug):
 
 
 def tag_filter(request, tag_title):
-    tag = Tag.objects.get(title=tag_title)
+    tag = get_object_or_404(Tag, title=tag_title)
 
     most_popular_tags = Tag.objects.popular()[:5]
 
